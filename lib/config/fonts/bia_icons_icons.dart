31613deeeecc1b2403b198946dae3da8f6c257dd
Edilberto Amado Perdomo <>
2023-06-19 01:04:33 -0500

    add flutter_riverpod, add go_router, add dio, add flutter_dotenv, add AppTheme add Custom Icons

---
+++ lib/config/fonts/bia_icons_icons.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/widgets.dart';
+
+class BiaIcons {
+  BiaIcons._();
+
+  static const _kFontFam = 'BiaIcons';
+  static const String? _kFontPkg = null;
+
+  static const IconData bia =
+      IconData(0xe801, fontFamily: _kFontFam, fontPackage: _kFontPkg);
+  static const IconData bia_1 =
+      IconData(0xe802, fontFamily: _kFontFam, fontPackage: _kFontPkg);
+  static const IconData marvelStudios =
+      IconData(0xe805, fontFamily: _kFontFam, fontPackage: _kFontPkg);
+}
